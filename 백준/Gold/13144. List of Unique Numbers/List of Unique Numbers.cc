778d6583390926cc0b56a67946f3fd593eda2085
BAI SUNG JUN <77558985+BBSSJJ@users.noreply.github.com>
2025-02-04 23:46:18 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 12 ms, Memory: 3308 KB -BaekjoonHub

---
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
@@ -0,0 +1,43 @@
+//13144
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int visited[100001];
+vector<int> v;
+int N;
+long long answer;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	
+	for(int i = 0 ; i < N; i++)
+	{
+		int n;
+		cin >> n;
+		v.push_back(n);
+	}
+
+	int st = 0;
+	int ed = 0;
+
+	
+	for (int st = 0; st < N; st++) {
+		while (ed < N) {
+			if (visited[v[ed]] == 1) break;
+			visited[v[ed]] = 1;
+			ed++;
+		}
+		answer += (ed - st);
+		visited[v[st]] = 0;
+	}
+	cout << answer;
+}
+
+
