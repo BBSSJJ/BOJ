249b2800ffa0f2733771262a8cbe69b87b1e11cb
BAI SUNG JUN <77558985+BBSSJJ@users.noreply.github.com>
2025-02-16 20:37:56 +0900

    [Gold IV] Title: 공유기 설치, Time: 116 ms, Memory: 3572 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -1,21 +1,17 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 vector<int> house;
+int N, C, x;
 
-int N, C, x; // 집 개수 N, 공유기 개수 C, 집좌표 x
-
-bool success(int hop)
-{
+bool success(int hop) {
     int start = 0;
     int count = 1;
 
-    for (int i = 0; i < N; i++)
-    {
-        if (house[start] + hop <= house[i])
-        {
+    for (int i = 0; i < N; i++) {
+        if (house[start] + hop <= house[i]) {
             start = i;
             count++;
         }
@@ -24,32 +20,27 @@ bool success(int hop)
     return count >= C;
 }
 
-int main()
-{
+int main() {
     cin >> N >> C;
 
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++) {
         cin >> x;
         house.push_back(x);
     }
 
     sort(house.begin(), house.end());
 
-    int hop_end = (house[N - 1] - house[0] + 1) * 2 / C;
-    int hop_start = 0;
+    int right = (house[N - 1] - house[0] + 1) * 2 / C;
+    int left = 0;
 
     int answer = 0;
-    while (hop_start <= hop_end)
-    {
-        int hop_mid = (hop_end + hop_start) / 2;
-        if (success(hop_mid))
-        {
-            answer = hop_mid;
-            hop_start = hop_mid + 1;
-        }
-        else{
-            hop_end = hop_mid - 1;
+    while (left <= right) {
+        int mid = (right + left) / 2;
+        if (success(mid)) {
+            answer = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
 
