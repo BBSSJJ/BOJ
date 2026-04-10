3b0f00a54f2291a284594b4217e7795fed367033
BAI SUNG JUN <77558985+BBSSJJ@users.noreply.github.com>
2026-04-10 13:18:02 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -1,4 +1,9 @@
-select d.DEPT_ID as DEPT_ID, d.DEPT_NAME_EN as DEPT_NAME_EN, round(avg(e.SAL), 0) as AVG_SAL
-from HR_EMPLOYEES as e inner join HR_DEPARTMENT as d on e.DEPT_ID = d.DEPT_ID
-group by e.DEPT_ID
-order by AVG_SAL desc
+# select d.DEPT_ID as DEPT_ID, d.DEPT_NAME_EN as DEPT_NAME_EN, round(avg(e.SAL), 0) as AVG_SAL
+# from HR_EMPLOYEES as e inner join HR_DEPARTMENT as d on e.DEPT_ID = d.DEPT_ID
+# group by e.DEPT_ID
+# order by AVG_SAL desc
+
+select hd.DEPT_ID as DEPT_ID, hd.DEPT_NAME_EN as DEPT_NAME_EN, round(avg(he.SAL), 0) as AVG_SAL
+from HR_DEPARTMENT hd inner join HR_EMPLOYEES he on hd.DEPT_ID = he.DEPT_ID
+group by hd.DEPT_ID
+order by AVG(he.SAL) desc
